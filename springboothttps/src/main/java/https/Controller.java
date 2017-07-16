dc3ecaba291d275aa01f70fe36e3dd5b3f6d318e
pruthvi <pruthvi0456@gmail.com>
2017-07-16 17:19:41 -0500

    initial

---
+++ springboothttps/src/main/java/https/Controller.java
@@ -0,0 +1,13 @@
+package https;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@RequestMapping("/")
+	public String print(){
+		return "Running";
+	}	
+}
---
+++ springboothttps/src/main/java/https/SpringboothttpsApplication.java
@@ -0,0 +1,12 @@
+package https;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothttpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboothttpsApplication.class, args);
+	}
+}
